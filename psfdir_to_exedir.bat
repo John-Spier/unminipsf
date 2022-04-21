07cb55d8fecd1d03ca97a1fff5d2e78ac89855b8
John-Spier <102952652+John-Spier@users.noreply.github.com>
2022-04-21 15:19:02 -0400

    Batch file to convert multiple sets at once
    psf2exe and upx are needed to run this

---
+++ psfdir_to_exedir.bat
@@ -0,0 +1,21 @@
+set bat_dir=%~dp0
+@echo off
+title PSF Sets to EXEs Converter
+color 8b
+cd /d %1
+md _UPX
+for /d %%A in (*) do (
+  if "%%A" NEQ "_UPX" md ".\_UPX\%%A"
+  cd "%%A"
+  if "%%A" NEQ "_UPX" call :do_dir "%%A"
+  cd..
+)
+pause
+goto :eof
+:do_dir
+  title %1 - PSF Sets to EXEs Converter
+  if exist *.psflib if not exist *.minipsf ren *.psf *.minipsf
+  for %%n in (*.minipsf) do python %bat_dir%\unminipsf_saga.py "%%n"
+  for %%p in (*.psf) do %bat_dir%\psf2exe.exe "%%p"
+  for %%q in (*.exe) do %bat_dir%\upx.exe --best --force -o"..\_UPX\%~1\%%~nq.exe" "%%q"
+goto :eof
